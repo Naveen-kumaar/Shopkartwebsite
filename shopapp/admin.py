e998a7292b754220e400b8f9ac308148e42d1e3f
Naveen-kumaar <navinsanjay2001@gmail.com>
2024-11-22 10:06:48 +0530

    uploading data an database

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -3,5 +3,11 @@ from .models import Category
 from .models import Product
 
 # Register your models here.
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('name','image','description')
+
+# admin.site.register(Category,CategoryAdmin)
+
 admin.site.register(Category)
 admin.site.register(Product)
